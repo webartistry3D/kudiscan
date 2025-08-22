5d71e4f2941f50c359d4a46f949f043bb7ef2a30
WebArtistry.3D <webartistry.io@gmail.com>
2025-08-22 13:07:30 +0100

    modified: server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -15,6 +15,14 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
+// Handle OPTIONS preflight for all routes
+app.options("*", cors({
+  origin: FRONTEND_URL,
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
+}));
+
 // --- JSON parsing ---
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
