22858bb9979e964b41d56c34e41dc4fc78515d98
WebArtistry.3D <webartistry.io@gmail.com>
2025-08-24 05:25:23 +0100

    Modified server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ const allowedOrigins = ["https://kudiscan.onrender.com"];
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
